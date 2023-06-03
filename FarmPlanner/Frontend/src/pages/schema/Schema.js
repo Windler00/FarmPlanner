61ee75d100d729c7b15890f1b8a78b285e53e764
Viktor <Windler00@gmail.com>
2023-06-04 01:16:19 +0600

    Revert "Fixed input size on modal schema page"
    This reverts commit 8cd5f90d3d14ebfca9c401fc46f077a6b4b93343.

--- FarmPlanner/Frontend/src/pages/schema/Schema.js
+++ FarmPlanner/Frontend/src/pages/schema/Schema.js
@@ -165,14 +165,10 @@ const Schema = observer(() =>{
             <Modal active={isActiveModal} setActive={setIsActiveModal}>
                 <div className={styles.ModalField}>
                     <div className={styles.FieldInfo}>
-                        <div>
-                            <strong>Ряд грядок</strong>
-                            <InputNumber min={0} defaultValue={schemaRow} onChange={setSchemaRow}></InputNumber>
-                        </div>
-                        <div>
-                            <strong>Номер</strong>
-                            <InputNumber min={0} defaultValue={schemaId} onChange={setSchemaId}></InputNumber>
-                        </div>
+                        <strong>Ряд грядок</strong>
+                        <InputNumber min={0} defaultValue={schemaRow} onChange={setSchemaRow}></InputNumber>
+                        <strong>Номер</strong>
+                        <InputNumber min={0} defaultValue={schemaId} onChange={setSchemaId}></InputNumber>
                         <CustomButton label={"Изменить"} onClick={() => {FieldStore.UpdateField(fieldId,fieldName,fieldDescription,fieldLength,fieldWidth,schemaRow,schemaId); setIsActiveModal(false)}}/>
                     </div>
                     <div className={styles.Rows}>
